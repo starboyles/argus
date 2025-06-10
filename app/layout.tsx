12a2774a8adbc91fdf8107510fdb79d43933a74c
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-06-10 02:09:44 +0000

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import { GeistSans, GeistMono } from 'geist/font'
 import './globals.css'
 
+export const metadata = {
+  title: 'Argus',
+  icons: {
+    icon: '/favicon.svg',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -8,10 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className="font-sans">
         {children}
       </body>
