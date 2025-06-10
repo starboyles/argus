7428d32b53cab170b67bcc15c7d955be217f3f6e
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-06-10 02:06:35 +0000

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="font-sans">
         {children}
       </body>
